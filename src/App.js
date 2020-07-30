ed9c66437a419fa4c9754db7bc67e51f24afe4ed
Student_GA126 <student_ga126@psxga126.lan>
2020-07-30 11:09:49 -0400

    Functioning next and back buttons

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 // Components
-// import Header from './components/Header';
+import Header from './components/Header';
 import Characters from './components/Characters';
 // import Worlds from './components/Worlds';
 
@@ -10,7 +10,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        {/* <Header /> */}
+        <Header />
         <Characters />
         {/* <Worlds /> */}
       </div>
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import axios from 'axios';
-import NextBtn from './NextBtn';
 
 class Characters extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            page: 'https://rickandmortyapi.com/api/character/?page=1',
-            info: [],
+            url: 'https://rickandmortyapi.com/api/character/?page=1',
             results: [],
+            info: [],
+            next: '',
+            prev: '',
         }
+
+        this.nextPage = this.nextPage.bind(this);
     }
 
     componentDidMount() {
-        axios.get(this.state.page)
+        axios.get(this.state.url)
             .then(res => {
-                this.setState({ results: res.data.results, info: res.data.info })
+                this.setState({
+                    results: res.data.results,
+                    info: res.data.info,
+                    next: res.data.info.next,
+                    prev: res.data.info.prev
+                })
                 console.log(`Next page is: ${this.state.info.next}`)
                 // console.log(this.state.results)
                 console.log(this.state.info)
@@ -24,16 +32,35 @@ class Characters extends React.Component {
             .catch(err => console.error(err.message))
     }
 
-
-    // ********** I need a function that will update state after a button click **********
-
     nextPage = () => {
-        this.setState({ page: this.state.info.next })
+        axios.get(this.state.next)
+            .then(res => {
+                this.setState({
+                    results: res.data.results,
+                    info: res.data.info,
+                    next: res.data.info.next,
+                    prev: res.data.info.prev
+                })
+            })
+            .catch(err => console.error(err.message))
+    }
+
+    prevPage = () => {
+        axios.get(this.state.prev)
+            .then(res => {
+                this.setState({
+                    results: res.data.results,
+                    info: res.data.info,
+                    next: res.data.info.next,
+                    prev: res.data.info.prev
+                })
+            })
+            .catch(err => console.error(err.message))
     }
 
     render() {
         return (
-            <div className='characters'>
+            <div className='characters' >
                 <h3>Characters</h3>
                 <p>Search bar under page title</p>
                 <p>Character list</p>
@@ -45,10 +72,8 @@ class Characters extends React.Component {
                         </li>
                     ))}
                 </ul>
-                {/* <NextBtn
-                    page={this.state.info.next}
-                /> */}
-                <button onClick={this.nextPage()}>Next</button>
+                <button onClick={(event) => this.prevPage(event)}>Back</button>
+                <button onClick={(event) => this.nextPage(event)}>Next</button>
 
             </div>
         )
