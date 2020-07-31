4af6d4eacbac362477a9f01614b2c8ccd272ee97
Student_GA126 <student_ga126@psxga126.lan>
2020-07-31 11:59:53 -0400

    Created Home coponent

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+    redner() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Home;
