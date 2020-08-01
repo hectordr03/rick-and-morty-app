3bb609a71ed47e9e2d4108bf9c1f739cb0951b68
Student_GA126 <student_ga126@psxga126.lan>
2020-08-01 08:46:20 -0400

    Locations initial style

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,12 +6,18 @@ import Button from 'react-bootstrap/Button'
 // Components
 import Welcome from '../components/Welcome'
 
+// Images
+import locationImg from '../assets/images/r&m-locations.png'
+
 class Home extends React.Component {
     render() {
         return (
             <div className='home'>
-                <Welcome />
-                {/* <Button>Click me</Button> */}
+                {/* <Welcome /> */}
+                <div className='location-link'>
+                    <h3>Wha-wha *burps* What's out there? Let's go check it out Morty</h3>
+                    <img src={locationImg} alt='' id='location-image'/>
+                </div>
             </div>
         )
     }
