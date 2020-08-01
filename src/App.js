25afd91250c2d2e24b0c32f8b45210ce80a3a957
Student_GA126 <student_ga126@psxga126.lan>
2020-08-01 16:07:39 -0400

    installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Home from './components/Home'
 import Characters from './components/Characters';
 import Worlds from './components/Worlds';
 
+// React Router
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
 class App extends React.Component {
   render() {
     return (
