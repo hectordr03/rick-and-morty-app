d4462871db865cf71f09246b6f988dfa0e88dd07
Student_GA126 <student_ga126@psxga126.lan>
2020-08-02 19:10:22 -0400

    Comments

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -10,6 +10,7 @@ import logo from '../assets/images/logo.png';
 const HeaderNav = () => {
     return (
         <div>
+            {/* I used a Bootstrap navbar because there were originally suppoed to be the "Characters" and "Worlds" links here but I removed them */}
             <Navbar className='header-nav'>
                 <Nav.Link href='/'>
                     <img src={logo} alt='' id='nav-logo' />
