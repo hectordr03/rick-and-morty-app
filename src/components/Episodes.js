8af9bb03f1b78ab8ad0303ee3c3b9c7dbd260166
Student_GA126 <student_ga126@psxga126.lan>
2020-08-02 17:13:26 -0400

    Pseudocode

--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -7,26 +7,27 @@ function Episodes(props) {
     return (
         <div>
             <h3 className='title'>Episode List</h3>
-        <Table className='episode-list'>
-            <thead>
-                <tr>
-                    <th>Episode Title</th>
-                    <th>Episode Number</th>
-                    <th>Air Date</th>
-                </tr>
-                
+            <Table className='episode-list'>
+                <thead>
+                    <tr>
+                        <th>Episode Title</th>
+                        <th>Episode Number</th>
+                        <th>Air Date</th>
+                    </tr>
+                </thead>
+
                 {/* Iterates through data in property "episodes" */}
                 {/* Creates a new row for each episode in episode */}
                 {/* Display data recieved from the API call for episodes inside table data cells */}
                 {props.episodes.map(ep => (
                     <tr>
-                        <td>{ ep.name }</td>
-                        <td>{ ep.episode }</td>
-                        <td>{ ep.air_date }</td>
+                        <td>{ep.name}</td>
+                        <td>{ep.episode}</td>
+                        <td>{ep.air_date}</td>
                     </tr>
                 ))}
-            </thead>
-        </Table>
+
+            </Table>
         </div>
     )
 }
