b849789af1abd1a9499e57919f1ee46fe5ea03c9
Student_GA126 <student_ga126@psxga126.lan>
2020-08-02 19:13:59 -0400

    Comments

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,6 +6,7 @@ function Welcome() {
     return (
         <Jumbotron className='welcome'>
             <h1>Wubba lubba dub-dub</h1>
+            {/* Text is from a Rick and Morty lorem ipsum website. I wanted to include a funny and charming welcome message but I couldn't think of anything so I used the lorem ipsum which is kind of funny to me. */}
             <p>"And"? What more do you want tacked on to this? I turned myself into a pickle, and 9/11 was an inside job?" I dunno, some people would pay top dollar for that kind of breakthrough. Listen, Morty, I hate to break it to you but what people call love is just a chemical reaction that compels animals to breed. What, cover me in gasoline and spiders? Fine, yeah, I'm in.</p>
         </Jumbotron>
     )
